de31b8bcd14dc0f36fdd36868ef2a57c1f6c75c5
olaseni <olaseniajayi5@gmail.com>
2023-11-25 13:26:28 +0100

    update

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import images from "../../constants";
+import images from "../../constants/images";
 
-const Comment = ((comment)) => {
+const Comment = ({comment}) => {
   return (
     <div className='flex flex-nowrap items-start gap-x-3 bg-[#F2F4F5] p-3 rounded-lg'>
         <img src={images[3]} alt="user profile" className='w-9 h-9 object-cover rounded-full' />
-
+        <div className='flex-1 flex flex-col '>
+            <h5 className='font-bold text-dark-hard tex-xs'>{comment.user.name}</h5>
+            <span className='text-xs text-dark-light'>
+            {new Date(comments.createdAt).toLocaleDateString("en-us", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                  hour: "2-digit",
+                })}
+            </span>
+            <p className='font-opensans mt-[10px] text-dark-light'>{comment.desc}</p>
+        </div>
     </div>
   )
 }
